59a2b9790708f811f7552b9668246ddc0be980a4
iqra <abdikarimiqra@gmail.com>
2025-07-03 21:43:25 +0300

    assign

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "M","brown", "Calm", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 23, "M","blue", "hyper", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 26, "F","green", "rowdy", FALSE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 20,"black", "M", "angelic", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 29, "F","white", "rush", FALSE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 30, "M","purple", "harsh", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 10, "F","yellow", "angry", FALSE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 10, "F","pink", "angry", FALSE);
